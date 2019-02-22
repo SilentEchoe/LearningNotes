827a7c7271e08a04c899293382fa3d936b472874
kai_wang <871779972@qq.com>
2019-02-22 09:28:00 +0800

    initialize

---
+++ LearningHard/LearningHard/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LearningHard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
