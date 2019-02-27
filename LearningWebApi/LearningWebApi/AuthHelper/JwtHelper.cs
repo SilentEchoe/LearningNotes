310af68cc10525561542629d541b59d8990b2ae2
kai_wang <871779972@qq.com>
2019-02-27 10:15:12 +0800

    add Common

--- LearningWebApi/LearningWebApi/AuthHelper/JwtHelper.cs
+++ LearningWebApi/LearningWebApi/AuthHelper/JwtHelper.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using Core.Common;
+using Microsoft.IdentityModel.Tokens;
 using System;
+using System.Collections.Generic;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 
