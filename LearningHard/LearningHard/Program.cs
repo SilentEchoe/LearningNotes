02312e7d5d05a354409def157da6ec3390e8d257
kai_wang <871779972@qq.com>
2019-03-02 13:49:06 +0800

    update Learning

--- LearningHard/LearningHard/Program.cs
+++ LearningHard/LearningHard/Program.cs
@@ -6,23 +6,16 @@ namespace LearningHard
     {
         static void Main(string[] args)
         {
-            // 值类型
-            int valueType = 3;
-
-            // 引用类型
-            string reftype = "abc";
-
             NestedVakueTypeInRef nestedVakueTypeInRef = new NestedVakueTypeInRef();
-
         }
     }
 
     public class NestedVakueTypeInRef
     {
         // valueType 作为引用类型的一部分被分配到托管堆上
-        private int valueType = 3;
+        private readonly int valueType = 3;
 
-        public void method()
+        public void Method()
         {
             // c被分配再线程堆栈上
             char c = 'c';
--- LearningHard/ScreenshotWindows/Form1.cs
+++ LearningHard/ScreenshotWindows/Form1.cs
@@ -19,10 +19,21 @@ namespace ScreenshotWindows
 
         private void Form1_Load(object sender, EventArgs e)
         {
-           
-            Bitmap bitmap = new Bitmap(Screen.AllScreens[0].Bounds.Width, Screen.AllScreens[0].Bounds.Height);
-            Graphics graphics = Graphics.FromImage(bitmap);
-            graphics.CopyFromScreen(new Point(0, 0), new Point(0, 0), new Size(Screen.AllScreens[0].Bounds.Width, Screen.AllScreens[0].Bounds.Height));
+
+            //Bitmap bitmap = new Bitmap(Screen.AllScreens[0].Bounds.Width, Screen.AllScreens[0].Bounds.Height);
+            //Graphics graphics = Graphics.FromImage(bitmap);
+            //graphics.CopyFromScreen(new Point(0, 0), new Point(0, 0), new Size(Screen.AllScreens[0].Bounds.Width, Screen.AllScreens[0].Bounds.Height));
+
+            string base64 = "EcwMgAAAAAAAAAAD/wIAIwAAMgBPRU0gICAgICAgICAgICAgBwAAAFRSLUZDODVTLU5CSyAgICAxQUJoB9BGJwIA/9YxMDBHU1I0ICAgICAgICAgMTUxMDExICAMAAAdNzQwLTA2MTQwNSBSRVYgMDFBUEYwNzI3NTQzMDEwNP8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////";
+
+
+            string base65 = "EcwMgAAAAAAAAAAD/wIAIwAAMgBPRU0gICAgICAgICAgICAgBwAAAFRSLUZDODVTLU5CSyAgICAxQUJoB9BGJwIA/9YxMDBHU1I0ICAgICAgICAgMTUxMDExICAMAAAdNzQwLTA2MTQwNSBSRVYgMDFBUEYwNzI3NTQzMDEwNP8AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAD/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////";
+            if (base64== base65)
+            {
+                MessageBox.Show("a");
+            }
+
+
            
         }
 
