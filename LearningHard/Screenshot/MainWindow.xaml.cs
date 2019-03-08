96a54666d31ea97a251c79cb7f3633f3f28b9fa5
kai_wang <871779972@qq.com>
2019-03-08 09:38:18 +0800

    update screenshot

--- LearningHard/Screenshot/MainWindow.xaml.cs
+++ LearningHard/Screenshot/MainWindow.xaml.cs
@@ -1,30 +1,53 @@
-﻿using CSharpWin_JD.CaptureImage;
+﻿using RisCaptureLib;
+using System;
+using System.Threading;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Forms;
 
 namespace Screenshot
 {
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly RisCaptureLib.ScreenCaputre screenCaputre = new RisCaptureLib.ScreenCaputre();
+        private Size? lastSize;
+
         public MainWindow()
         {
             InitializeComponent();
+            screenCaputre.ScreenCaputred += OnScreenCaputred;
+            screenCaputre.ScreenCaputreCancelled += OnScreenCaputreCancelled;
         }
 
+        private void OnScreenCaputreCancelled(object sender, EventArgs e)
+        {
+            Show();
+            Focus();
+        }
+
+        private void OnScreenCaputred(object sender, ScreenCaputredEventArgs e)
+        {
+            //set last size
+            lastSize = new Size(e.Bmp.Width, e.Bmp.Height);
+            Show();
+            //test
+            var bmp = e.Bmp;
+            Clipboard.SetImage(e.Bmp);
+        }
+
+        private void OnScreenCapturedBitmap(object sender, ScreenCaputredBitmapEventArgs e)
+        {
+            System.Drawing.Bitmap bitmap = e.bBitMap;
+        }
         private void Btn_Screenshot(object sender, RoutedEventArgs e)
         {
-            CaptureImageTool capture = new CaptureImageTool();
-            if (capture.ShowDialog() == DialogResult)
-            {
-                Image image = capture.Image;
-                //pictureBox1.Width = image.Width;
-                //pictureBox1.Height = image.Height;
-                //pictureBox1.Image = image;
-            }
+            Hide();
+            Thread.Sleep(300);
+            screenCaputre.StartCaputre(30, lastSize);
         }
     }
 }
--- LearningHard/ScreenshotWindows/Form1.cs
+++ LearningHard/ScreenshotWindows/Form1.cs
@@ -1,12 +1,4 @@
-﻿using CSharpWin_JD.CaptureImage;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 using System.Windows.Forms;
 
 namespace ScreenshotWindows
@@ -20,15 +12,7 @@ namespace ScreenshotWindows
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            CaptureImageTool capture = new CaptureImageTool();
-            if (capture.ShowDialog() == DialogResult.OK)
-            {
-                Image image = capture.Image;
-                //pictureBox1.Width = image.Width;
-                //pictureBox1.Height = image.Height;
-                //pictureBox1.Image = image;
-            }
-
+           
             //Bitmap bitmap = new Bitmap(Screen.AllScreens[0].Bounds.Width, Screen.AllScreens[0].Bounds.Height);
             //Graphics graphics = Graphics.FromImage(bitmap);
             //graphics.CopyFromScreen(new Point(0, 0), new Point(0, 0), new Size(Screen.AllScreens[0].Bounds.Width, Screen.AllScreens[0].Bounds.Height));
