07d030df650c90ffde5ce8177fc27d0ce614ebbd
kai_wang <871779972@qq.com>
2019-03-08 09:26:00 +0800

    add RisCaptureLib

---
+++ LearningHard/RisCaptureLib/ToolBarControl.xaml.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RisCaptureLib
+{
+    /// <summary>
+    /// ToolBarControl.xaml 的交互逻辑
+    /// </summary>
+    public partial class ToolBarControl : UserControl
+    {
+
+        //确定事件
+        public delegate void OKEventHander();
+        public event OKEventHander OnOK;
+        //取消事件
+        public delegate void CancelEventHander();
+        public event CancelEventHander OnCancel;
+        //保存事件
+        public delegate void SaveCaptureEventHander();
+        public event SaveCaptureEventHander OnSaveCapture;
+
+        public ToolBarControl()
+        {
+            InitializeComponent();
+        }
+        
+        private void buttonSave_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (OnSaveCapture!=null)
+            {
+                OnSaveCapture();
+            }
+            buttonComplete_Click(sender, e);
+
+
+
+        }
+
+        private void buttonCancel_Click(object sender, RoutedEventArgs e)
+        {
+            if (OnCancel != null)
+            {
+                OnCancel();
+            }
+        }
+
+        private void buttonComplete_Click(object sender, RoutedEventArgs e)
+        {
+            if (OnOK != null)
+            {
+                OnOK();
+            }
+        }
+    }
+}
