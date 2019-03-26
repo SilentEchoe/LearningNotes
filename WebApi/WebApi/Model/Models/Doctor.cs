304117f9c696f841c09ed312210177324eddaac2
kai_wang <871779972@qq.com>
2019-03-26 09:54:33 +0800

    add port

---
+++ WebApi/WebApi/Model/Models/Doctor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.Models
+{
+    public class Doctor
+    {
+        public int Id { get; set; }
+
+
+    }
+}
---
+++ WebApi/WebApi/Repository/sugar/BaseDBConfig.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Repository.sugar
+{
+    public class BaseDBConfig
+    {
+        public static string ConnectionString { get; set; }
+    }
+}
