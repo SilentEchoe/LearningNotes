14bb1bf4f65612b224d6ef4dbf729b0ea6761c4a
kai_wang <871779972@qq.com>
2019-03-26 10:35:43 +0800

    框架搭建

--- WebApi/WebApi/WebApi/Controllers/ValuesController.cs
+++ WebApi/WebApi/WebApi/Controllers/ValuesController.cs
@@ -1,14 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using IServices;
 using Microsoft.AspNetCore.Mvc;
+using Model.Models;
+using Services;
 
 namespace WebApi.Controllers
 {
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
-       
+        [HttpGet("{id}", Name = "Get")]
+        public async Task<List<Doctor>> Get(int id)
+        {
+            IDoctorServices advertisementServices = new DoctorServices();
+
+            return await advertisementServices.Query(d => d.Id == id);
+        }
     }
 }
