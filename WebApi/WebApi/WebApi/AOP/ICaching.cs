3fc63bfbb1976ff8ebb8acde07e25e788c179833
kai_wang <871779972@qq.com>
2019-03-30 14:56:04 +0800

    “add“add 缓存类

---
+++ WebApi/WebApi/WebApi/AOP/ICaching.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi.AOP
+{
+    // 缓存接口
+    interface ICaching
+    {
+        object Get(string cacheKey);
+
+        void Set(string cacheKey, object cacheValue);
+    }
+}
