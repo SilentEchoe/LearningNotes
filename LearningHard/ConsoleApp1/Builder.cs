e624448f5f41bf0dcbf167b35aad15e26fd6a21f
kai_wang <871779972@qq.com>
2019-05-16 18:06:44 +0800

    建造者模式构建

---
+++ LearningHard/ConsoleApp1/Builder.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Customer
+{
+    /// <summary>
+    /// 抽象建造者
+    /// </summary>
+   public class Builder
+    {
+        //装CPU
+        public abstract void BuildPartCPU();
+
+
+
+
+    }
+}
---
+++ LearningHard/ConsoleApp1/Director.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Customer
+{
+    public class Director
+    {
+      
+
+
+    }
+}
