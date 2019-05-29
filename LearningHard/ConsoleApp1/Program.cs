2f3a4459763df2d868b0f6688097e553f089d539
kai_wang <871779972@qq.com>
2019-05-29 16:04:06 +0800

    feat:测试建造者模式

--- LearningHard/ConsoleApp1/Program.cs
+++ LearningHard/ConsoleApp1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Customer;
 
 namespace ConsoleApp1
 {
@@ -6,7 +7,25 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 创建指挥者
+            Director director = new Director();
+            
+            Builder b1 = new  ConcreteBuilder1();
+            Builder b2 = new  ConcreteBuilder2();
+
+            director.Construst(b1);
+
+            // 组装完，组装人员搬来组装好的电脑
+            Computer computer1 = b1.GetComputer();
+            computer1.Show();
+
+            // 老板叫员工去组装第二台电脑
+            director.Construst(b2);
+            Computer computer2 = b2.GetComputer();
+            computer2.Show();
+
+
+            Console.Read();
         }
     }
 }
