68356798e93376171b699c2466c61b98133e54d9
kai_wang <871779972@qq.com>
2019-05-29 15:59:57 +0800

    feat:更改建造者模式

---
+++ LearningHard/ConsoleApp1/ConcreteBuilder2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Customer
+{
+
+
+    public class ConcreteBuilder2 : Builder
+    {
+        Computer computer = new Computer();
+
+        public override void BuildPartCPU()
+        {
+            computer.Add("CPU2");
+        }
+
+
+        public override void BuildPartMainBoard()
+        {
+
+            computer.Add("Main board2");
+        }
+
+        public override Computer GetComputer()
+        {
+            return computer;
+        }
+
+    }
+}
