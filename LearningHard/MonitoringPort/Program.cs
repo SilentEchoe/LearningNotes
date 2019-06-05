7f7562260d86d3e13e3527f4be1bd5c21afc7eba
kai_wang <871779972@qq.com>
2019-06-05 10:00:43 +0800

    feat:MonitoringPort

---
+++ LearningHard/MonitoringPort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.NetworkInformation;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MonitoringPort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Port: 56789 status: " + (PortInUse(56789) ? "in use" : "not in use"));
+            Console.Read();
+
+        }
+
+
+        public static bool PortInUse(int port)
+        {
+            bool inUse = false;
+
+            IPGlobalProperties ipProperties = IPGlobalProperties.GetIPGlobalProperties();
+            IPEndPoint[] ipEndPoints = ipProperties.GetActiveTcpListeners();
+
+            foreach (IPEndPoint endPoint in ipEndPoints)
+            {
+                if (endPoint.Port == port)
+                {
+                    inUse = true;
+                    break;
+                }
+            }
+            return inUse;
+        }
+
+
+
+    }
+}
