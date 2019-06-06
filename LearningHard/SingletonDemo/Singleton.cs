5785ae45c4ffa4cec2a9019d63944104355f99a5
kai_wang <871779972@qq.com>
2019-06-06 18:32:23 +0800

    feat:更改redis

---
+++ LearningHard/SingletonDemo/Singleton.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDemo
+{
+    public sealed class Singleton
+    {
+        private static int counter = 0;
+
+        private Singleton()
+        {
+            counter++;
+            Console.WriteLine("Counter value"+ counter);
+        }
+        private  static  readonly  Singleton singleInstance = new Singleton();
+
+        public static Singleton GetInstance
+        {
+            get { return singleInstance; }
+
+        }
+
+        public void PrintDetails(string message)
+        {
+            Console.WriteLine(message);
+        }
+
+    }
+}
---
+++ LearningHard/Test2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Test2
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+
+
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
