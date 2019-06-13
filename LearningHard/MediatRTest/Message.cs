d5db69d6676af6f1c0e3f04ced0bd67a5cb1679f
kai_wang <871779972@qq.com>
2019-06-13 17:44:53 +0800

    feat:add Message Class

---
+++ LearningHard/MediatRTest/Message.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediatRTest
+{
+    public class Message
+    {
+        public string CustomMessage { get; set; }
+
+        public void EmailMessage()
+        {
+            Console.WriteLine($"{CustomMessage}: Email Message sent");
+        }
+
+        public void SmsMessage()
+        {
+            Console.WriteLine($"{CustomMessage}:Sms Message sent");
+        }
+
+
+
+    }
+}
