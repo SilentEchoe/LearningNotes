7ffa3b1eb9e60e909fa7023cd0643b4662858b78
kai_wang <871779972@qq.com>
2019-06-13 17:51:10 +0800

    feat:add EmailMessageCommand class

---
+++ LearningHard/MediatRTest/EmailMessageCommand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediatRTest
+{
+    public class EmailMessageCommand:IMessageCommand
+    {
+        private Message oMessage;
+
+        public EmailMessageCommand(Message oMessage)
+        {
+            this.oMessage = oMessage;
+
+        }
+
+        public void DoAction()
+        {
+            oMessage.EmailMessage();
+        }
+
+
+
+    }
+}
