700b16d64735715409c89509a465cd3590cae8ef
kai_wang <871779972@qq.com>
2019-06-13 17:48:21 +0800

    feat:add Broker class

---
+++ LearningHard/MediatRTest/IMessageCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MediatRTest
+{
+    public interface IMessageCommand
+    {
+        void DoAction();
+    }
+
+    public class Broker
+    {
+        public void SendMessage(IMessageCommand command)
+        {
+            command.DoAction();
+        }
+
+    }
+
+}
