cf20f309325e1844335948a740979b5303b6f0f1
kai_wang <871779972@qq.com>
2019-06-13 17:40:50 +0800

    feat:Test star

--- LearningHard/Test2/Program.cs
+++ LearningHard/Test2/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
 
 namespace Test2
 {
@@ -6,6 +9,25 @@ namespace Test2
     {
         public static void Main(string[] args)
         {
+            //Process.Start(@"C:\Users\Lenovo\Desktop\ceshi\FS_Feedback\FS_Feedback.exe");
+
+
+
+
+            //Process[] app = Process.GetProcessesByName("FS_Feedback");
+            //if (app.Length > 0)
+            //{
+            //    Console.WriteLine("已启动");   
+            //    return;
+            //}
+
+            DirectoryInfo info = new DirectoryInfo(@"C:\FS\FS_Service\Log\");
+
+            DateTime a =  DateTime.Today;
+            DateTime b =  a.AddDays(1);
+            var allLines = info.GetFiles("*.LOG")
+                .Where(p => p.CreationTime.Date > a && p.CreationTime.Date < b);
+                
 
 
 
