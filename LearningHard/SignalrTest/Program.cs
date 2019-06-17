a33ff68d9f3b0b1089de5f7bce30392508f61824
kai_wang <871779972@qq.com>
2019-06-17 14:12:34 +0800

    feat:add signalr

---
+++ LearningHard/SignalrTest/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Reflection;
+
+namespace SignalrTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+    public static class HubServer
+    {
+        private static IDisposable _signalR;
+
+        public static IDisposable InitialHubServer()
+        {
+            if (_signalR != null)
+            {
+                return _signalR;
+            }
+
+            try
+            {
+                //_signalR = WebApp.Start(HubConfiguration.URL);
+            }
+            catch (TargetInvocationException ex)
+            {
+                string a = ex.ToString();
+            }
+
+            return _signalR;
+        }
+    }
+
+
+
+}
