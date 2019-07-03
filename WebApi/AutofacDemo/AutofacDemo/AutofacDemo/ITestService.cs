f52416fbc331e5a300624232fd36a46f49158672
kai_wang <871779972@qq.com>
2019-07-03 17:03:47 +0800

    feat:添加接口

---
+++ WebApi/AutofacDemo/AutofacDemo/AutofacDemo/ITestService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AutofacDemo
+{
+    public interface ITestService
+    {
+        Guid MyProperty { get; }
+        List<string> GetList(string a);
+    }
+}
--- WebApi/AutofacDemo/AutofacDemo/AutofacDemo/Startup.cs
+++ WebApi/AutofacDemo/AutofacDemo/AutofacDemo/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
@@ -22,9 +24,16 @@ namespace AutofacDemo
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public IServiceProvider IServiceProvider(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc();
+            services.AddDbContext<BloggingContext>();
+            services.AddDirectoryBrowser();
+            var containerBuilder = new ContainerBuilder();
+            containerBuilder.RegisterModule<DefaultModule>();
+            containerBuilder.Populate(services);
+            var container = containerBuilder.Build();
+            return new AutofacServiceProvider(container);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
