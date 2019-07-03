57b94868cf79853964b40fb64cc538774ca7404d
kai_wang <871779972@qq.com>
2019-07-03 14:16:41 +0800

    feat:autofac

---
+++ LearningHard/AutofacDemo/GuidAppServiceBase.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutofacDemo
+{
+    public class GuidAppServiceBase : IGuidAppService
+    {
+        private readonly Guid _item;
+
+        public GuidAppServiceBase()
+        {
+            _item = Guid.NewGuid();
+        }
+
+        public Guid GuidItem()
+        {
+            return _item;
+        }
+    }
+
+    public class GuidTransientAppService : GuidAppServiceBase, IGuidTransientAppService
+    {
+    }
+
+    public class GuidScopedAppService : GuidAppServiceBase, IGuidScopedAppService
+    {
+    }
+
+    public class GuidSingletonAppService : GuidAppServiceBase, IGuidSingletonAppService
+    {
+    }
+
+}
---
+++ LearningHard/AutofacDemo/IGuidAppService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AutofacDemo
+{
+    public interface IGuidAppService
+    {
+        Guid GuidItem();
+    }
+
+    public interface IGuidTransientAppService : IGuidAppService
+    {
+    }
+
+    public interface IGuidScopedAppService : IGuidAppService
+    {
+    }
+
+    public interface IGuidSingletonAppService : IGuidAppService
+    {
+    }
+}
