dcdc7f9f1a1f0b2cd0b6864b708a2d34c3da29cf
kai_wang <871779972@qq.com>
2019-07-11 18:20:22 +0800

    feat:更改脚本文件

--- LearningHard/ExcelTest/Program.cs
+++ LearningHard/ExcelTest/Program.cs
@@ -21,28 +21,38 @@ namespace ExcelTest
             Console.ReadKey();
         }
 
-        static string binFilePath = @"C:\Users\Lenovo\Desktop\脚本\云端中性码\";
+        static string binFilePath = @"C:\Users\Lenovo\Desktop\脚本\GE\云端中性码\";
 
 
         public static void Addcompatibility()
         {
             // 获取数据库型号名
-            var modelLits = GetModelLists();
+            //var modelLits = GetModelLists();
 
-            var list = OpenExcel2(@"C:\Users\Lenovo\Desktop\脚本\Generic.xls");
+            var list = OpenExcel2(@"C:\Users\Lenovo\Desktop\脚本\Ge.xls");
 
             foreach (var item in list)
             {
-                string key = string.Empty;
+
+
+             var a = attrKey(item);
+
+
+             Console.WriteLine($"型号名：{item.Modelname}，attry_ket{a}");
+
+
+             //Console.WriteLine($"型号名：{item.Modelname}，属性一{item.Value1}，属性二{item.Value2}，属性三{item.Value3}，属性四{item.Value4}，属性五{item.Value5}，属性六{item.Value6}");
+
+                //string key = string.Empty;
 
                 //string binPath = binFilePath + item.Base64;
                 //Console.WriteLine(BinToBase64(binPath));
-                var modalid = modelLits.Where(p => p.modal_name == item.Modelname).FirstOrDefault();
+                //var modalid = modelLits.Where(p => p.modal_name == item.Modelname).FirstOrDefault();
 
 
              
 
-                Console.WriteLine(item.Modelname +":"+ key);
+                //Console.WriteLine(item.Modelname +":"+ key);
 
 
 
@@ -130,31 +140,56 @@ namespace ExcelTest
             return null;
         }
 
-        public static string attrKey(string value1,string value2,string attrValue)
+        public static string attrKey(model2 model)
         {
+            string attr_key = string.Empty;
+            string attr_value = string.Empty;
+            if (model.Value1!="0")
+            {
+                attr_key += "1,";
+                attr_value += model.Value1 + ",";
+            }
+            if(model.Value2!="0")
+            {
+                attr_key += "2,";
+            }
+            if (model.Value3 != "0")
+            {
+                attr_key += "3,";
+            }
+            if (model.Value4 != "0")
+            {
+                attr_key += "4,";
+            }
+            if (model.Value5 != "0")
+            {
+                attr_key += "5,";
+            }
+            if (model.Value6 != "0")
+            {
+                attr_key += "6,";
+            }
+
+            if (attr_key == "")
+            {
+                attr_key = "0,0";
+            }
             
-            string key = string.Empty;
-            if (value1 != "0")
+
+
+            string[] attr_keya = attr_key.Split(',');
+            if (attr_keya.Length==2 && attr_keya[1]=="")
             {
-                key += "1,";
-                attrValue = value1;
+                attr_key += "0";
             }
-            else
+            if (attr_keya.Length == 3)
             {
-                key += "0,";
-            }
-            if (value2 != "0")
-            {
-                key += "2";
-                attrValue = value1;
-            }
-            else
-            {
-                key += "0";
+                attr_key = attr_key.Substring(0, attr_key.Length - 1);
+                
             }
 
-            return key;
 
+            return attr_key;
         }
 
 
@@ -230,7 +265,7 @@ namespace ExcelTest
                                                              //int columnsint = mySheet.UsedRange.Cells.Columns.Count;//得到列数
 
 
-                //取得数据范围区域 (不包括标题列) 
+                //型号名
                 Range rng1 = ws.Cells.get_Range("A2", "A" + rowsint);   //item
 
 
@@ -316,19 +351,40 @@ namespace ExcelTest
                 // 属性二
                 Range rng3 = ws.Cells.get_Range("C2", "C" + rowsint);
 
-                // Bin文件
-                Range rng4 = ws.Cells.get_Range("F2", "F" + rowsint);
+                // 属性三
+                Range rng4 = ws.Cells.get_Range("D2", "D" + rowsint);
+
+                // 属性四
+                Range rng5 = ws.Cells.get_Range("E2", "E" + rowsint);
+
+                // 属性五
+                Range rng6 = ws.Cells.get_Range("F2", "F" + rowsint);
+
+
+                // 属性六
+                Range rng7 = ws.Cells.get_Range("G2", "G" + rowsint);
+
+                // bin文件
+                Range rng8 = ws.Cells.get_Range("J2", "J" + rowsint);
 
 
 
 
+                // 型号名
+                object[,] arryItem = (object[,])rng1.Value2;
 
-                object[,] arryItem = (object[,])rng1.Value2;   //get range's value
-                object[,] arryItem2 = (object[,])rng2.Value2;
+               
+                object[,] arryItem1 = (object[,])rng2.Value2;
 
-                object[,] arryItem3 = (object[,])rng3.Value2;   //get range's value
+                object[,] arryItem2 = (object[,])rng3.Value2;   //get range's value
 
-                object[,] arryItem4 = (object[,])rng4.Value2;
+                object[,] arryItem3 = (object[,])rng4.Value2;
+
+
+                object[,] arryItem4 = (object[,])rng5.Value2;
+                object[,] arryItem5 = (object[,])rng6.Value2;
+                object[,] arryItem6 = (object[,])rng7.Value2;
+                object[,] arryItem7 = (object[,])rng8.Value2;
 
                 //将新值赋给一个数组
                 string[,] arry = new string[rowsint - 1, 2];
@@ -336,9 +392,17 @@ namespace ExcelTest
                 {
                     model2 model = new model2();
                     model.Modelname = arryItem[i, 1].ToString();
-                    model.Value1 = arryItem2[i, 1].ToString();
-                    model.Value2 = arryItem3[i, 1].ToString();
-                    model.Base64 = arryItem4[i, 1].ToString() + ".bin";
+                    model.Value1 = arryItem1[i, 1].ToString();
+                    model.Value2 = arryItem2[i, 1].ToString();
+
+                    model.Value3 = arryItem3[i, 1].ToString();
+
+                    model.Value4 = arryItem4[i, 1].ToString();
+
+                    model.Value5 = arryItem5[i, 1].ToString();
+                    model.Value6 = arryItem6[i, 1].ToString();
+
+                    model.Base64 = arryItem7[i, 1].ToString() + ".bin";
 
                     modelsList.Add(model);
 
--- LearningHard/ExcelTest/model.cs
+++ LearningHard/ExcelTest/model.cs
@@ -28,6 +28,11 @@ namespace ExcelTest
 
         public string Value2 { get; set; }
 
+        public string Value3 { get; set; }
+        public string Value4 { get; set; }
+        public string Value5 { get; set; }
+        public string Value6 { get; set; }
+
         public string Base64 { get; set; }
 
 
