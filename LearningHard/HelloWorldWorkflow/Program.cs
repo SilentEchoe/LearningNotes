6ac3254484101f9c97db18e196e7fc0e870a6a77
kai_wang <871779972@qq.com>
2019-07-19 15:40:27 +0800

    feat:创建HelloWorldWorkflow

---
+++ LearningHard/HelloWorldWorkflow/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HelloWorldWorkflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
