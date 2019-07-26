6466fe320754cd5e0aca031ce60e0b8589f7336d
kai_wang <871779972@qq.com>
2019-07-26 15:26:02 +0800

    TS:构造函数

--- Typescript/hello.js
+++ Typescript/hello.js
@@ -1,7 +1,20 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 function sayHelloTwo(person) {
-    return 'Hello,' + person;
+    return "Hello," + person;
 }
-var userinfo = 'Hi';
+var userinfo = "Hi";
 //console.log(sayHello(user))
 //alert('hello world in TypeScript!');
 function add(left, right) {
@@ -9,21 +22,22 @@ function add(left, right) {
 }
 function areat(shape) {
     var area = shape.width * shape.height;
-    return "Im:" + shape.name + 'With area' + area + "cm squared";
+    return "Im:" + shape.name + "With area" + area + "cm squared";
 }
 //alert(area({name:"Readaw",width:20,height:30}))
 var shape = {
     name: "rectangle",
     popup: function () {
         var _this = this;
-        console.log('This inside popup():' + this.name);
+        console.log("This inside popup():" + this.name);
         setTimeout(function () {
-            console.log('This inside setTimeout():' + _this.name);
-            console.log('Im a ' + _this.name + '!');
+            console.log("This inside setTimeout():" + _this.name);
+            console.log("Im a " + _this.name + "!");
         }, 3000);
     }
 };
 var Class2 = /** @class */ (function () {
+    // 构造函数
     function Class2(name, width, height) {
         this.area = width * height;
         this.color = "Pink";
@@ -36,5 +50,56 @@ var Class2 = /** @class */ (function () {
 shape.popup();
 var square = new Class2("square", 30, 30);
 console.log(square.shoutout());
-console.log('Area of Shape: ' + square.area);
-console.log('Color of Shape: ' + square.color);
+console.log("Area of Shape: " + square.area);
+console.log("Color of Shape: " + square.color);
+var Animal = /** @class */ (function () {
+    function Animal(TheName) {
+        this.name = TheName;
+    }
+    Animal.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 0; }
+        console.log(this.name + " moved " + distanceInMeters + "m.");
+    };
+    return Animal;
+}());
+// extends 关键字创建子类
+var Snake = /** @class */ (function (_super) {
+    __extends(Snake, _super);
+    function Snake(name) {
+        return _super.call(this, name) || this;
+    }
+    Snake.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 5; }
+        console.log("Slithering...");
+        _super.prototype.move.call(this, distanceInMeters);
+    };
+    return Snake;
+}(Animal));
+var Horese = /** @class */ (function (_super) {
+    __extends(Horese, _super);
+    function Horese(name) {
+        return _super.call(this, name) || this;
+    }
+    Horese.prototype.move = function (distanceInMeters) {
+        if (distanceInMeters === void 0) { distanceInMeters = 45; }
+        console.log("Galloping...");
+        _super.prototype.move.call(this, distanceInMeters);
+    };
+    return Horese;
+}(Animal));
+var sam = new Snake("Sammy the Python");
+var tom = new Horese("Tommy the Palomino");
+sam.move();
+tom.move();
+var Greeter = /** @class */ (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        return "Hello," + this.greeting;
+    };
+    return Greeter;
+}());
+var greeter;
+greeter = new Greeter("World");
+console.log(greeter.greet());
--- Typescript/hello.ts
+++ Typescript/hello.ts
@@ -1,130 +1,123 @@
-
-
 function sayHelloTwo(person: string) {
-    return 'Hello,' + person;
+  return "Hello," + person;
 }
 
-let userinfo = 'Hi'
+let userinfo = "Hi";
 
 //console.log(sayHello(user))
 //alert('hello world in TypeScript!');
 
-
-
-function add(left:number,right:number):number
-{
-    return left + right;
-
+function add(left: number, right: number): number {
+  return left + right;
 }
 
 interface Shape {
-    name: string;
-    width: number;
-    height: number;
-    color?: string;
+  name: string;
+  width: number;
+  height: number;
+  color?: string;
 }
 
 function areat(shape: Shape) {
-    var area = shape.width * shape.height;
-    return "Im:" + shape.name + 'With area' + area + "cm squared"
+  var area = shape.width * shape.height;
+  return "Im:" + shape.name + "With area" + area + "cm squared";
 }
-
 
 //alert(area({name:"Readaw",width:20,height:30}))
 
-var shape = 
-{
-    name:"rectangle",
-    popup:function()
-    {
-        console.log('This inside popup():'+ this.name)
+var shape = {
+  name: "rectangle",
+  popup: function() {
+    console.log("This inside popup():" + this.name);
 
-        setTimeout(()=>
-        {
-            console.log('This inside setTimeout():'+ this.name);
-            console.log('Im a '+ this.name + '!');
+    setTimeout(() => {
+      console.log("This inside setTimeout():" + this.name);
+      console.log("Im a " + this.name + "!");
+    }, 3000);
+  }
+};
 
-        },3000)
-   
-    }
+class Class2 {
+  area: number;
+  color: string;
 
+  // 构造函数
+  constructor(name: string, width: number, height: number) {
+    this.area = width * height;
+    this.color = "Pink";
+  }
+
+  shoutout() {
+    return "I'm " + this.color + this.area + " cm squared.";
+  }
 }
-
-
-class Class2
-{
-    area : number;
-    color: string;
-
-    // 构造函数
-    constructor(name:string,width:number,height:number)
-    {
-        this.area = width * height;
-        this.color = "Pink";
-    }
-
-    shoutout()
-    {
-        return "I'm " + this.color + this.area + " cm squared.";
-    }
-
-}
-
 
 shape.popup();
 
 var square = new Class2("square", 30, 30);
 
-console.log( square.shoutout() );
-console.log( 'Area of Shape: ' + square.area );
-console.log( 'Color of Shape: ' + square.color );
+console.log(square.shoutout());
+console.log("Area of Shape: " + square.area);
+console.log("Color of Shape: " + square.color);
 
+class Animal {
+  name: string;
+  constructor(TheName: string) {
+    this.name = TheName;
+  }
 
-class Animal
-{
-    name:string;
-    constructor(TheName:string)
-    {
-        this.name = TheName;
-    }
-
-    move(distanceInMeters:number= 0)
-    {
-        console.log(`${this.name} moved ${distanceInMeters}m.`);
-    }
-
+  move(distanceInMeters: number = 0) {
+    console.log(`${this.name} moved ${distanceInMeters}m.`);
+  }
 }
-
 
 // extends 关键字创建子类
-class Snake extends Animal
-{
-    constructor(name:string){super(name);}
-    move(distanceInMeters = 5)
-    {
-        console.log("Slithering...")
-        super.move(distanceInMeters);
-    }
+class Snake extends Animal {
+  constructor(name: string) {
+    super(name);
+  }
+  move(distanceInMeters = 5) {
+    console.log("Slithering...");
+    super.move(distanceInMeters);
+  }
 }
 
-
-class Horese extends Animal
-{
-    constructor(name:string){super(name);}
-    move(distanceInMeters = 45)
-    {
-        console.log("Galloping...")
-        super.move(distanceInMeters);
-    }
+class Horese extends Animal {
+  constructor(name: string) {
+    super(name);
+  }
+  move(distanceInMeters = 45) {
+    console.log("Galloping...");
+    super.move(distanceInMeters);
+  }
 }
 
 
 let sam = new Snake("Sammy the Python");
-
-let tom:Animal = new Horese("Tommy the Palomino");
+let tom: Animal = new Horese("Tommy the Palomino");
 
 sam.move();
-tom.move()
+tom.move();
+
+class Greeter
+{
+    greeting:string;
+
+    constructor(message:string)
+    {
+        this.greeting = message;
+    }
+
+    greet()
+    {
+        return "Hello," + this.greeting;
+    }
+    
+}
+
+let greeter:Greeter;
+greeter = new Greeter("World");
+console.log(greeter.greet());
 
 
 
