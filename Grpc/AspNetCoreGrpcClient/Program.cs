7f64703a80c9b56b6027efb8e49dde88383cc08a
kai_wang <871779972@qq.com>
2019-09-27 16:01:26 +0800

    客户端更改信息

--- Grpc/AspNetCoreGrpcClient/Program.cs
+++ Grpc/AspNetCoreGrpcClient/Program.cs
@@ -1,12 +1,22 @@
 ﻿using System;
+using System.Threading.Tasks;
+using AspNetCoregRpcService;
+using Google.Protobuf.WellKnownTypes;
+using Grpc.Net.Client;
 
 namespace AspNetCoreGrpcClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            var client = new Greeter.GreeterClient(channel);
+            var reply = await client.SayHelloAsync(
+                new HelloRequest { Name = "晓晨" });
+            Console.WriteLine("调用Greeter服务 : " + reply.Message);
+
+            Console.ReadKey();
         }
     }
 }
