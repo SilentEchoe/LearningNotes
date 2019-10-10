8b25da7b122142c69f84108d59f09a3e24f02ed8
kai_wang <871779972@qq.com>
2019-10-10 11:16:55 +0800

    Grpc:双向流

--- Grpc/AspNetCoregRpcService/Services/LuCatService.cs
+++ Grpc/AspNetCoregRpcService/Services/LuCatService.cs
@@ -1,5 +1,6 @@
 ﻿using Google.Protobuf.WellKnownTypes;
 using Grpc.Core;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,10 +10,49 @@ namespace AspNetCoregRpcService.Services
 {
     public class LuCatService: LuCat.LuCatBase
     {
+        private readonly ILogger<LuCatService> _logger;
+
         // 声明喵的种类
         private static readonly List<string> Cats = new List<string>() { "英短银渐层", "英短金渐层", "美短", "蓝猫", "狸花猫", "橘猫" };
 
         private static readonly Random Rand = new Random(DateTime.Now.Millisecond);
+
+        public LuCatService(ILogger<LuCatService> logger)
+        {
+            _logger = logger;
+        }
+
+
+        public override async Task BathTheCat(IAsyncStreamReader<BathTheCatReq> requestStream, IServerStreamWriter<BathTheCatResp> responseStream, ServerCallContext context)
+        {
+            var bathQueue = new Queue<int>();
+            while (await requestStream.MoveNext())
+            {
+                //将要洗澡的猫加入队列
+                bathQueue.Enqueue(requestStream.Current.Id);
+
+                _logger.LogInformation($"Cat {requestStream.Current.Id} Enqueue.");
+            }
+
+            //遍历队列开始洗澡
+            while (bathQueue.TryDequeue(out var catId))
+            {
+                await responseStream.WriteAsync(new BathTheCatResp() { Message = $"铲屎的成功给一只{Cats[catId]}洗了澡！" });
+
+                await Task.Delay(500);//此处主要是为了方便客户端能看出流调用的效果
+            }
+
+
+        }
+
+        public override Task<CountCatResult> Count(Empty request, ServerCallContext context)
+        {
+            return Task.FromResult(new CountCatResult()
+            {
+                Count = Cats.Count
+            });
+        }
+
 
         public override Task<SuckingCatResult> SuckingCat(Empty request, ServerCallContext context) 
         {
