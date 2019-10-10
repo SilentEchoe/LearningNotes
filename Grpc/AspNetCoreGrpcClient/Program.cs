e0285337f51560418026aa301c6f349b43da42dd
kai_wang <871779972@qq.com>
2019-10-10 11:49:20 +0800

    grpc:建立客户端双向流访问

--- Grpc/AspNetCoreGrpcClient/Program.cs
+++ Grpc/AspNetCoreGrpcClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using AspNetCoregRpcService;
 using Google.Protobuf.WellKnownTypes;
+using Grpc.Core;
 using Grpc.Net.Client;
 
 namespace AspNetCoreGrpcClient
@@ -10,15 +11,41 @@ namespace AspNetCoreGrpcClient
     {
         static async Task Main(string[] args)
         {
-            var channel = GrpcChannel.ForAddress("https://localhost:5001");
-            var client = new Greeter.GreeterClient(channel);
-            var reply = await client.SayHelloAsync(
-                new HelloRequest { Name = "晓晨" });
-            Console.WriteLine("Greeter 服务返回数据: " + reply.Message);
+            //var channel = GrpcChannel.ForAddress("https://localhost:5001");
+            //var client = new Greeter.GreeterClient(channel);
+            //var reply = await client.SayHelloAsync(
+            //    new HelloRequest { Name = "晓晨" });
+            //Console.WriteLine("Greeter 服务返回数据: " + reply.Message);
 
+            var channel = GrpcChannel.ForAddress("https://localhost:5001");
             var catClient = new LuCat.LuCatClient(channel);
-            var catReply = await catClient.SuckingCatAsync(new Empty());
-            Console.WriteLine("调用撸猫服务：" + catReply.Message);
+            //获取猫总数
+            var catCount = await catClient.CountAsync(new Empty());
+            Console.WriteLine($"一共{catCount.Count}只猫。");
+            var rand = new Random(DateTime.Now.Millisecond);
+
+            var bathCat = catClient.BathTheCat();
+            //定义接收吸猫响应逻辑
+            var bathCatRespTask = Task.Run(async () =>
+            {
+                await foreach (var resp in bathCat.ResponseStream.ReadAllAsync())
+                {
+                    Console.WriteLine(resp.Message);
+                }
+            });
+            //随机给10个猫洗澡
+            for (int i = 0; i < 10; i++)
+            {
+                await bathCat.RequestStream.WriteAsync(new BathTheCatReq() { Id = rand.Next(0, catCount.Count) });
+            }
+            //发送完毕
+            await bathCat.RequestStream.CompleteAsync();
+            Console.WriteLine("客户端已发送完10个需要洗澡的猫id");
+            Console.WriteLine("接收洗澡结果：");
+            //开始接收响应
+            await bathCatRespTask;
+
+            Console.WriteLine("洗澡完毕");
 
             Console.ReadKey();
         }
--- Grpc/AspNetCoreGrpcClient/obj/Debug/netcoreapp3.0/LuCatGrpc.cs
+++ Grpc/AspNetCoreGrpcClient/obj/Debug/netcoreapp3.0/LuCatGrpc.cs
@@ -17,6 +17,9 @@ namespace AspNetCoregRpcService {
 
     static readonly grpc::Marshaller<global::Google.Protobuf.WellKnownTypes.Empty> __Marshaller_google_protobuf_Empty = grpc::Marshallers.Create((arg) => global::Google.Protobuf.MessageExtensions.ToByteArray(arg), global::Google.Protobuf.WellKnownTypes.Empty.Parser.ParseFrom);
     static readonly grpc::Marshaller<global::AspNetCoregRpcService.SuckingCatResult> __Marshaller_LuCat_SuckingCatResult = grpc::Marshallers.Create((arg) => global::Google.Protobuf.MessageExtensions.ToByteArray(arg), global::AspNetCoregRpcService.SuckingCatResult.Parser.ParseFrom);
+    static readonly grpc::Marshaller<global::AspNetCoregRpcService.BathTheCatReq> __Marshaller_LuCat_BathTheCatReq = grpc::Marshallers.Create((arg) => global::Google.Protobuf.MessageExtensions.ToByteArray(arg), global::AspNetCoregRpcService.BathTheCatReq.Parser.ParseFrom);
+    static readonly grpc::Marshaller<global::AspNetCoregRpcService.BathTheCatResp> __Marshaller_LuCat_BathTheCatResp = grpc::Marshallers.Create((arg) => global::Google.Protobuf.MessageExtensions.ToByteArray(arg), global::AspNetCoregRpcService.BathTheCatResp.Parser.ParseFrom);
+    static readonly grpc::Marshaller<global::AspNetCoregRpcService.CountCatResult> __Marshaller_LuCat_CountCatResult = grpc::Marshallers.Create((arg) => global::Google.Protobuf.MessageExtensions.ToByteArray(arg), global::AspNetCoregRpcService.CountCatResult.Parser.ParseFrom);
 
     static readonly grpc::Method<global::Google.Protobuf.WellKnownTypes.Empty, global::AspNetCoregRpcService.SuckingCatResult> __Method_SuckingCat = new grpc::Method<global::Google.Protobuf.WellKnownTypes.Empty, global::AspNetCoregRpcService.SuckingCatResult>(
         grpc::MethodType.Unary,
@@ -25,27 +28,24 @@ namespace AspNetCoregRpcService {
         __Marshaller_google_protobuf_Empty,
         __Marshaller_LuCat_SuckingCatResult);
 
+    static readonly grpc::Method<global::AspNetCoregRpcService.BathTheCatReq, global::AspNetCoregRpcService.BathTheCatResp> __Method_BathTheCat = new grpc::Method<global::AspNetCoregRpcService.BathTheCatReq, global::AspNetCoregRpcService.BathTheCatResp>(
+        grpc::MethodType.DuplexStreaming,
+        __ServiceName,
+        "BathTheCat",
+        __Marshaller_LuCat_BathTheCatReq,
+        __Marshaller_LuCat_BathTheCatResp);
+
+    static readonly grpc::Method<global::Google.Protobuf.WellKnownTypes.Empty, global::AspNetCoregRpcService.CountCatResult> __Method_Count = new grpc::Method<global::Google.Protobuf.WellKnownTypes.Empty, global::AspNetCoregRpcService.CountCatResult>(
+        grpc::MethodType.Unary,
+        __ServiceName,
+        "Count",
+        __Marshaller_google_protobuf_Empty,
+        __Marshaller_LuCat_CountCatResult);
+
     /// <summary>Service descriptor</summary>
     public static global::Google.Protobuf.Reflection.ServiceDescriptor Descriptor
     {
       get { return global::AspNetCoregRpcService.LuCatReflection.Descriptor.Services[0]; }
-    }
-
-    /// <summary>Base class for server-side implementations of LuCat</summary>
-    [grpc::BindServiceMethod(typeof(LuCat), "BindService")]
-    public abstract partial class LuCatBase
-    {
-      /// <summary>
-      ///定义吸猫方法
-      /// </summary>
-      /// <param name="request">The request received from the client.</param>
-      /// <param name="context">The context of the server-side call handler being invoked.</param>
-      /// <returns>The response to send back to the client (wrapped by a task).</returns>
-      public virtual global::System.Threading.Tasks.Task<global::AspNetCoregRpcService.SuckingCatResult> SuckingCat(global::Google.Protobuf.WellKnownTypes.Empty request, grpc::ServerCallContext context)
-      {
-        throw new grpc::RpcException(new grpc::Status(grpc::StatusCode.Unimplemented, ""));
-      }
-
     }
 
     /// <summary>Client for LuCat</summary>
@@ -115,28 +115,75 @@ namespace AspNetCoregRpcService {
       {
         return CallInvoker.AsyncUnaryCall(__Method_SuckingCat, null, options, request);
       }
+      /// <summary>
+      ///定义给猫洗澡双向流rpc
+      /// </summary>
+      /// <param name="headers">The initial metadata to send with the call. This parameter is optional.</param>
+      /// <param name="deadline">An optional deadline for the call. The call will be cancelled if deadline is hit.</param>
+      /// <param name="cancellationToken">An optional token for canceling the call.</param>
+      /// <returns>The call object.</returns>
+      public virtual grpc::AsyncDuplexStreamingCall<global::AspNetCoregRpcService.BathTheCatReq, global::AspNetCoregRpcService.BathTheCatResp> BathTheCat(grpc::Metadata headers = null, global::System.DateTime? deadline = null, global::System.Threading.CancellationToken cancellationToken = default(global::System.Threading.CancellationToken))
+      {
+        return BathTheCat(new grpc::CallOptions(headers, deadline, cancellationToken));
+      }
+      /// <summary>
+      ///定义给猫洗澡双向流rpc
+      /// </summary>
+      /// <param name="options">The options for the call.</param>
+      /// <returns>The call object.</returns>
+      public virtual grpc::AsyncDuplexStreamingCall<global::AspNetCoregRpcService.BathTheCatReq, global::AspNetCoregRpcService.BathTheCatResp> BathTheCat(grpc::CallOptions options)
+      {
+        return CallInvoker.AsyncDuplexStreamingCall(__Method_BathTheCat, null, options);
+      }
+      /// <summary>
+      ///定义统计猫数量简单rpc
+      /// </summary>
+      /// <param name="request">The request to send to the server.</param>
+      /// <param name="headers">The initial metadata to send with the call. This parameter is optional.</param>
+      /// <param name="deadline">An optional deadline for the call. The call will be cancelled if deadline is hit.</param>
+      /// <param name="cancellationToken">An optional token for canceling the call.</param>
+      /// <returns>The response received from the server.</returns>
+      public virtual global::AspNetCoregRpcService.CountCatResult Count(global::Google.Protobuf.WellKnownTypes.Empty request, grpc::Metadata headers = null, global::System.DateTime? deadline = null, global::System.Threading.CancellationToken cancellationToken = default(global::System.Threading.CancellationToken))
+      {
+        return Count(request, new grpc::CallOptions(headers, deadline, cancellationToken));
+      }
+      /// <summary>
+      ///定义统计猫数量简单rpc
+      /// </summary>
+      /// <param name="request">The request to send to the server.</param>
+      /// <param name="options">The options for the call.</param>
+      /// <returns>The response received from the server.</returns>
+      public virtual global::AspNetCoregRpcService.CountCatResult Count(global::Google.Protobuf.WellKnownTypes.Empty request, grpc::CallOptions options)
+      {
+        return CallInvoker.BlockingUnaryCall(__Method_Count, null, options, request);
+      }
+      /// <summary>
+      ///定义统计猫数量简单rpc
+      /// </summary>
+      /// <param name="request">The request to send to the server.</param>
+      /// <param name="headers">The initial metadata to send with the call. This parameter is optional.</param>
+      /// <param name="deadline">An optional deadline for the call. The call will be cancelled if deadline is hit.</param>
+      /// <param name="cancellationToken">An optional token for canceling the call.</param>
+      /// <returns>The call object.</returns>
+      public virtual grpc::AsyncUnaryCall<global::AspNetCoregRpcService.CountCatResult> CountAsync(global::Google.Protobuf.WellKnownTypes.Empty request, grpc::Metadata headers = null, global::System.DateTime? deadline = null, global::System.Threading.CancellationToken cancellationToken = default(global::System.Threading.CancellationToken))
+      {
+        return CountAsync(request, new grpc::CallOptions(headers, deadline, cancellationToken));
+      }
+      /// <summary>
+      ///定义统计猫数量简单rpc
+      /// </summary>
+      /// <param name="request">The request to send to the server.</param>
+      /// <param name="options">The options for the call.</param>
+      /// <returns>The call object.</returns>
+      public virtual grpc::AsyncUnaryCall<global::AspNetCoregRpcService.CountCatResult> CountAsync(global::Google.Protobuf.WellKnownTypes.Empty request, grpc::CallOptions options)
+      {
+        return CallInvoker.AsyncUnaryCall(__Method_Count, null, options, request);
+      }
       /// <summary>Creates a new instance of client from given <c>ClientBaseConfiguration</c>.</summary>
       protected override LuCatClient NewInstance(ClientBaseConfiguration configuration)
       {
         return new LuCatClient(configuration);
       }
-    }
-
-    /// <summary>Creates service definition that can be registered with a server</summary>
-    /// <param name="serviceImpl">An object implementing the server-side handling logic.</param>
-    public static grpc::ServerServiceDefinition BindService(LuCatBase serviceImpl)
-    {
-      return grpc::ServerServiceDefinition.CreateBuilder()
-          .AddMethod(__Method_SuckingCat, serviceImpl.SuckingCat).Build();
-    }
-
-    /// <summary>Register service method with a service binder with or without implementation. Useful when customizing the  service binding logic.
-    /// Note: this method is part of an experimental API that can change or be removed without any prior notice.</summary>
-    /// <param name="serviceBinder">Service methods will be bound by calling <c>AddMethod</c> on this object.</param>
-    /// <param name="serviceImpl">An object implementing the server-side handling logic.</param>
-    public static void BindService(grpc::ServiceBinderBase serviceBinder, LuCatBase serviceImpl)
-    {
-      serviceBinder.AddMethod(__Method_SuckingCat, serviceImpl == null ? null : new grpc::UnaryServerMethod<global::Google.Protobuf.WellKnownTypes.Empty, global::AspNetCoregRpcService.SuckingCatResult>(serviceImpl.SuckingCat));
     }
 
   }
