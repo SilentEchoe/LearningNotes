edc67d6875b5e1adf9d2041fbf5667472527cd70
kai_wang <871779972@qq.com>
2019-10-11 14:57:10 +0800

    dockerApi：进行容器操作设置

--- DockerApi/DockerServiceApi/DockerServiceApi/Controllers/ValuesController.cs
+++ DockerApi/DockerServiceApi/DockerServiceApi/Controllers/ValuesController.cs
@@ -8,13 +8,15 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace DockerServiceApi.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("/api")]
     [ApiController]
     public class ValuesController : ControllerBase
     {
         // GET api/values
+
         [HttpGet]
-        public async Task<ActionResult<string>> Get()
+        [Route("GetAllDockerRun")]
+        public async Task<ActionResult<string>> GetAllDockerRun()
         {
 
             try
@@ -27,11 +29,13 @@ namespace DockerServiceApi.Controllers
                              {
                                  Limit = 10,
                              });
+                // 查询正在运行的容器数量
+                var dockerCount = containers.Count().ToString();
 
-               
-                string a = containers[0].Image; 
 
-                return containers.Count().ToString() + "——" + a;
+                string a = containers[0].Image;
+
+                return $"Docker容器数量运行数量{dockerCount}";
             }
             catch (Exception ex)
             {
@@ -40,6 +44,52 @@ namespace DockerServiceApi.Controllers
             }
         }
 
-      
+
+        [HttpGet]
+        [Route("AddDocker")]
+        public async Task<ActionResult<string>> AddDocker()
+        {
+            try
+            {
+                DockerClient client = new DockerClientConfiguration(
+                        new Uri("unix:///var/run/docker.sock"))
+                        .CreateClient();
+
+              
+
+                await client.Containers.StartWithConfigContainerExecAsync("0f3e07c0138f", new ContainerExecStartParameters
+                {
+                    Env = new List<string> { "ACCEPT_EULA=Y", $"SA_PASSWORD=null" },
+                });
+
+
+                // 在容器中执行CMD 命令
+                //var execResponse = await client.Containers.ExecCreateContainerAsync(
+                //"307ef337fbfd", new ContainerExecCreateParameters()
+                //{
+                //    Detach = false,
+                //    Tty = true,
+                //    Cmd = new[]
+                //    {
+                //        "mkdir", "/tmp/abc"
+                //    }
+                //});
+
+
+                //await client.Containers.ExecCreateContainerAsync("0f3e07c0138f", containerExecCreate);
+                return "执行完毕";
+            }
+            catch (Exception ex)
+            {
+
+                return ex.ToString();
+            }
+
+
+        }
+
+
+
+
     }
 }
