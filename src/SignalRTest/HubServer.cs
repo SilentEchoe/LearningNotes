f2bc9e05aafabb861bba6924fa7ca82390875238
kai_wang <871779972@qq.com>
2019-11-21 16:09:03 +0800

    .net core Signalr

---
+++ src/SignalRTest/HubServer.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.Owin.Hosting;
+using Owin;
+using System;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.Owin.Cors;
+namespace SignalRTest
+{
+    public static class HubServer
+    {
+
+        private static IDisposable _signalR;
+
+        public static IDisposable InitialHubServer()
+        {
+            if (_signalR != null)
+            {
+                return _signalR;
+            }
+
+            try
+            {
+               
+                _signalR = WebApp.Start("http://localhost:56789");
+            }
+            catch (TargetInvocationException ex)
+            {
+                string a = ex.ToString();
+            }
+
+            return _signalR;
+        }
+
+    }
+
+
+
+    /// <summary>
+    /// Used by OWIN's startup process.
+    /// </summary>
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.UseCors(CorsOptions.AllowAll);
+            app.MapSignalR();
+        }
+    }
+
+
+    public class MyHub : Hub
+    {
+        public static bool IsExecute = true;
+      
+        public void Send(string name, string message)
+        {
+            switch (name)
+            {
+                
+                case "":
+                    {
+                      
+                        Clients.Client(Context.ConnectionId).addMessage(name, "1");
+                        break;
+                    }
+
+
+                default:
+                    {
+                        Clients.Client(Context.ConnectionId).addMessage(name, Context.ConnectionId);
+                        break;
+                    }
+            }
+        }
+
+
+
+
+     
+
+        // 重连
+        public override Task OnReconnected()
+        {
+           
+            return base.OnReconnected();
+        }
+
+        // 连接
+        public override Task OnConnected()
+        {
+            
+            var cConnected = base.OnConnected();
+            return cConnected;
+        }
+
+
+       
+
+
+    }
+
+
+}
---
+++ src/SignalRTest/Program.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.SignalR;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.AspNetCore.SignalR.Client;
+using System;
+
+namespace SignalRTest
+{
+    class Program
+    {
+
+        private IDisposable _hub;
+        private IHubContext _hubContext;
+
+       
+        static void Main(string[] args)
+        {
+
+            _hubContext = GlobalHost.ConnectionManager.GetHubContext<MyHub>();
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
