b7fa62b14e672ca878419c934df25dc2d09f5e59
kai_wang <871779972@qq.com>
2020-01-15 13:01:01 +0800

    添加计时器

--- src/BinFileUploading/MainWindow.xaml.cs
+++ src/BinFileUploading/MainWindow.xaml.cs
@@ -14,20 +14,48 @@ namespace BinFileUploading
         // 获取程序所在路径
         readonly string BinPath = AppDomain.CurrentDomain.BaseDirectory;
 
-        string BinFilePath = string.Empty;
-
+        System.Windows.Threading.DispatcherTimer dispatcherTimer = new System.Windows.Threading.DispatcherTimer();
+        System.Windows.Forms.FolderBrowserDialog folder = new System.Windows.Forms.FolderBrowserDialog();
         public MainWindow()
         {
             InitializeComponent();
+            #region 定义定时器
+            dispatcherTimer.Tick += DispatcherTimer_Tick;
+            dispatcherTimer.Interval = new TimeSpan(0, 0, 1, 0);
+            #endregion
         }
 
         private void Btn_Upload_Click(object sender, RoutedEventArgs e)
         {
+            UploadBin();
+        }
+
+
+        private void Btn_selectPath_Click(object sender, RoutedEventArgs e)
+        {
+            folder.ShowDialog();//打开文件夹会话
+
+            this.BinFilePath.Content = folder.SelectedPath;
+            //BinFilePath = folder.SelectedPath;//获取文件夹路径
+
+        }
+
+        // 定时器触发的事件
+        private void DispatcherTimer_Tick(object sender, EventArgs e)
+        {
+            UploadBin();
+        }
+
+        private void UploadBin()
+        {
+
+            int Count = 0;
             // 选择Bin文件文件夹
-            string Path = @"D:\work\Fsbox\2015-07\07-04";
+            string Path = this.BinFilePath.Content.ToString();
 
             // 获取文件夹名
             string[] folders = Directory.GetDirectories(Path);
+            
 
             foreach (var item in folders)
             {
@@ -41,7 +69,7 @@ namespace BinFileUploading
                         string filename = System.IO.Path.GetFileName(bins);
 
                         //三个参数分别是源文件路径，存储路径，若存储路径有相同文件是否替换
-                        File.Copy(bins, BinPath+ @"BinFile\"+filename, true);
+                        File.Copy(bins, BinPath + @"BinFile\" + filename, true);
                     }
                 }
                 Guid g = Guid.NewGuid();
@@ -50,37 +78,31 @@ namespace BinFileUploading
                 // 压缩文件夹
                 ZipHelper.CreateZip(BinPath + "BinFile", zipPath);
                 // 将zip 上传到服务器上
-                if (httpUtlity.HttpFileUpload(zipPath)) 
+                if (httpUtlity.HttpFileUpload(zipPath))
                 {
-                    MessageBox.Show("上传成功");
+                    Count++;
                     // 清理已上传的BIN文件
                     fileUtility.DeleteFolder(BinPath + "BinFile");
-
                 }
                 else
                 {
-                    MessageBox.Show("上传失败");
+
                     break;
                 }
+            }
 
-
+            if (folders.Length == Count)
+            {
+                MessageBox.Show("上传成功");
+            }
+            else
+            {
+                MessageBox.Show("实际上传Bin文件数量：" + Count);
             }
 
 
-
-
-           
-
-
-
         }
 
-        System.Windows.Forms.FolderBrowserDialog folder = new System.Windows.Forms.FolderBrowserDialog();
-        private void Btn_selectPath_Click(object sender, RoutedEventArgs e)
-        {
-            folder.ShowDialog();//打开文件夹会话
-            BinFilePath = folder.SelectedPath;//获取文件夹路径
 
-        }
     }
 }
