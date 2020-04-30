51013a61507818d44cbfdff1b87ce1538d8d700b
kai_wang <871779972@qq.com>
2020-04-30 14:00:51 +0800

    添加Services

---
+++ src/Grpc/GrpcServer.Web/Services/MyEmployeeService.cs
@@ -0,0 +1,28 @@
+﻿using Grpc.Core;
+using GrpcServer.Web.Protos;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GrpcServer.Web.Services
+{
+    public class MyEmployeeService: EmployeeService.EmployeeServiceBase
+    {
+        private readonly ILogger<MyEmployeeService> _logger;
+
+        public MyEmployeeService(ILogger<MyEmployeeService> logger) 
+        {
+            _logger = logger;
+        }
+
+        public override Task<EmployeeResponse> GetByNo(GetByNoRequest request, ServerCallContext context)
+        {
+            return base.GetByNo(request, context);
+        }
+
+
+
+    }
+}
