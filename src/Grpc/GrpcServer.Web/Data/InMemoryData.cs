04b5c4707ddd3730952a65262fd8ed854d201013
kai_wang <871779972@qq.com>
2020-04-30 14:05:03 +0800

    添加data

---
+++ src/Grpc/GrpcServer.Web/Data/InMemoryData.cs
@@ -0,0 +1,41 @@
+﻿using GrpcServer.Web.Protos;
+using System.Collections.Generic;
+
+namespace GrpcServer.Web.Data
+{
+    public class InMemoryData
+    {
+        public static List<Employee> Employees = new List<Employee>
+        {
+            new Employee
+            {
+                Id =1 ,
+                No=1994,
+                FirstName = "Chandler",
+                LastName = "Bing",
+                Salary = 2200
+            },
+        new Employee
+            {
+                Id =2 ,
+                No=1995,
+                FirstName = "LASH",
+                LastName = "ROAP",
+                Salary = 2500
+            },
+        new Employee
+            {
+                Id =3 ,
+                No=1996,
+                FirstName = "AWDhandler",
+                LastName = "AGAWg",
+                Salary = 2800
+            },
+        };
+
+
+
+
+
+    }
+}
