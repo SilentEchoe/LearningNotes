cd457c4a4691f8ad6ba229683ecf6d85005624fe
kai_wang <871779972@qq.com>
2020-04-30 14:49:55 +0800

    grpcclient Requst

--- src/Grpc/GrpcClient/Program.cs
+++ src/Grpc/GrpcClient/Program.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Grpc.Net.Client;
+using GrpcServer.Web.Protos;
+using System;
+using System.Threading.Tasks;
 
 namespace GrpcClient
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+           using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+           var client = new EmployeeService.EmployeeServiceClient(channel);
+
+            var response = await client.GetByNoAsync(new GetByNoRequest
+            {
+                No = 1994
+            });
+
+            Console.WriteLine($"Response messages :{ response }");
+            Console.WriteLine("press any key to exit.");
+            Console.ReadKey();
+            
         }
     }
 }
