7e5ccadf16b88738360ee2e503c558c4a89c71b2
kai_wang <871779972@qq.com>
2020-05-20 22:52:54 +0800

    更改.net core grpc

--- src/Grpc/GrpcClient/Program.cs
+++ src/Grpc/GrpcClient/Program.cs
@@ -1,6 +1,5 @@
 ﻿using Grpc.Core;
 using Grpc.Net.Client;
-using GrpcServer.Web.Protos;
 using System;
 using System.Threading.Tasks;
 
@@ -8,29 +7,29 @@ namespace GrpcClient
 {
     class Program
     {
-        static async Task Main(string[] args)
+        static  void Main(string[] args)
         {
-            var md = new Metadata
-            {
-                {"username","dave" },
-                { "role","administrator"}
+           // var md = new Metadata
+           // {
+           //     {"username","dave" },
+           //     { "role","administrator"}
 
-            };
-
-
+           // };
 
 
-           using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-           var client = new EmployeeService.EmployeeServiceClient(channel);
 
-            var response = await client.GetByNoAsync(new GetByNoRequest
-            {
-                No = 1994
-            },md);
 
-            Console.WriteLine($"Response messages :{ response }");
-            Console.WriteLine("press any key to exit.");
-            Console.ReadKey();
+           //using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+           //var client = new EmployeeService.EmployeeServiceClient(channel);
+
+           // var response = await client.GetByNoAsync(new GetByNoRequest
+           // {
+           //     No = 1994
+           // },md);
+
+           // Console.WriteLine($"Response messages :{ response }");
+           // Console.WriteLine("press any key to exit.");
+           // Console.ReadKey();
             
         }
     }
